3dc5a575a1723e2b64d520a3908112748685e583
Ranjith M <126071064+Ranjith-M7@users.noreply.github.com>
2024-03-25 20:33:38 +0530

    Updated Media Query

--- Flower Shop/assets/js/script.js
+++ Flower Shop/assets/js/script.js
@@ -1,20 +1,28 @@
 const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
-  header.classList.toggle("sticky", window.scrollY > 0);
+  const isHeaderSticky = window.scrollY > 0;
+
+  header.classList.toggle("sticky", isHeaderSticky);
+
+  if (isHeaderSticky) {
+    const headerLogo = header.querySelector(".header__logo");
+    headerLogo.style.width = "30px";
+  } else {
+    const headerLogo = header.querySelector(".header__logo");
+    headerLogo.style.width = ""; // Revert to the default width
+  }
 });
 
 const headerMenu = document.querySelector(".header__menu");
 const menuBtn = document.querySelector(".menu-btn");
 const headerMenuItems = document.querySelectorAll("li a");
 
-
-menuBtn.addEventListener("click",()=>{
+menuBtn.addEventListener("click", () => {
   headerMenu.classList.toggle("show");
-})
+});
 
-headerMenuItems.forEach(item=>{
-  item.addEventListener("click",()=>{
+headerMenuItems.forEach((item) => {
+  item.addEventListener("click", () => {
     headerMenu.classList.remove("show");
-  })
-})
-
+  });
+});
