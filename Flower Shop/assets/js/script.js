b4e3201f20cf032d3243c7d08b9a44c8913bf714
Ranjith M <126071064+Ranjith-M7@users.noreply.github.com>
2024-03-25 16:10:22 +0530

    Update script.js

--- Flower Shop/assets/js/script.js
+++ Flower Shop/assets/js/script.js
@@ -1,28 +1,19 @@
-const message = " mango is a yellow color,apple is a red color   ";
-
-// split the message using commas
-const parts = message.split(",");
-
-// exclude these words from capitalize
-const excludeWords = ["is", "a"];
-
-// function to capitalize the first letter of the word, exclude certain words
-function capitalizedFirstLetter(word) {
-  // check if the word is in the excludeWords array
-  if (excludeWords.includes(word.toLowerCase())) {
-    return word;
-  }
-  // capitalize the first letter of the word
-  return word.charAt(0).toUpperCase() + word.slice(1);
-}
-
-// capitalize the first letter of each word
-const capitalizedParts = parts.map((part) => {
-  // split each part using spaces
-  const words = part.trim().split(" ");
-  const capitalizedWords = words.map((word) => capitalizedFirstLetter(word));
-  return capitalizedWords.join(" ");
+const header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-const capitalizedMessage = capitalizedParts.join(",");
-console.log(capitalizedMessage);
+const headerMenu = document.querySelector(".header__menu");
+const menuBtn = document.querySelector(".menu-btn");
+const headerMenuItems = document.querySelectorAll("li a");
+
+
+menuBtn.addEventListener("click",()=>{
+  headerMenu.classList.toggle("show");
+})
+
+headerMenuItems.forEach(item=>{
+  item.addEventListener("click",()=>{
+    headerMenu.classList.remove("show");
+  })
+})
