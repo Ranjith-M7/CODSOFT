c0e01e248ec376ab617d2301a70b8fee8d2753fc
Ranjith M <126071064+Ranjith-M7@users.noreply.github.com>
2024-03-30 20:50:42 +0530

    basic files

---
+++ Portfolio/README.md
@@ -0,0 +1,63 @@
+
+# Portfolio
+
+## Description
+- **Purpose:** 
+- **Specialization:** 
+- **Responsiveness:** 
+- **Design:** 
+
+## Tech Stack
+
+**Client:** HTML, CSS, JavaScript
+
+## Demo
+
+Check out the demo video for a walkthrough of the project.
+[Watch the Demo Video]()
+
+## Website Link
+[Click here]() to visit the website .
+
+
+## Screenshot
+
+![App Screenshot]()
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone 
+```
+
+Go to the project directory
+
+```bash
+  cd Portfolio
+```
+
+
+
+## Author
+
+- [@Ranjith-M7](https://github.com/Ranjith-M7)
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
+          <nav class="menu">
+            <!-- Menu Links -->
+            <div class="icon-list">
+              <a href="index.html#home"><i class="fa fa-fw fa-home"></i><span>Home</span></a>
+              <a href="index.html#about"><i class="fa fa-fw fa-quote-left"></i><span>About</span></a>
+              <a href="index.html#service"><i class="fa fa-fw fa-globe"></i><span>Service</span></a>
+              <a href="index.html#portfolio"><i class="fa fa-fw fa-picture-o"></i><span>Portfolio</span></a>
+              <a href="index.html#blog"><i class="fa fa-fw fa-rss"></i><span>Blog</span></a>
+              <a href="index.html#contact"><i class="fa fa-fw fa-envelope-o"></i><span>Contact</span></a>
+            </div>
+          </nav>
---
+++ Portfolio/assets/js/script.js
@@ -0,0 +1,74 @@
+// Wait for the window to load
+window.addEventListener("load", function () {
+  // Get the preloader element
+  const preloader = document.getElementById("preloader");
+  preloader.style.opacity = "0";
+  // After fading out, remove it from the DOM
+  setTimeout(function () {
+    // Fade out the preloader
+
+    preloader.parentNode.removeChild(preloader);
+  }, 500);
+});
+
+// Add event listerner to set the heaver sticky when scrolling
+window.addEventListener("scroll",()=>{
+  document.querySelector("header").classList.toggle("sticky",scrollY > 0);
+})
+
+// Change hamburger menu icon depends on nav bar opens or not
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const menuList = document.querySelector(".menu-list");
+let isOpen = false;
+
+hamburgerMenu.addEventListener("click", () => {
+  isOpen = !isOpen;
+  if (isOpen) {
+    hamburgerMenu.innerHTML = `<i class="fa-brands fa-xing"></i>`;
+  } else {
+    hamburgerMenu.innerHTML = `<i class="fa fa-bars"></i>`;
+  }
+
+  menuList.classList.toggle("show");
+});
+
+// When click the nav link removes the nav bar
+const navLinks = document.querySelectorAll(".menu-list a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.querySelector(".menu-list").classList.toggle("show");
+    isOpen = false;
+    if(!isOpen){
+      hamburgerMenu.innerHTML = `<i class="fa fa-bars"></i>`;
+    }
+  });
+});
+
+// Function to run the animation
+function runAnimation() {
+  const text = document.querySelector(".job");
+  const words = text.innerText.split(",");
+  let currentIndex = 0;
+
+  function animateWords() {
+    // Fade out the current word before updating
+    text.style.opacity = "0";
+
+    setTimeout(() => {
+      // Update the text content with the current word
+      text.textContent = words[currentIndex];
+
+      // Fade in the new word
+      text.style.opacity = "1";
+
+      // Move to the next word after a short delay
+      setTimeout(() => {
+        currentIndex = (currentIndex + 1) % words.length;
+        animateWords();
+      }, 1000);
+    }, 500);
+  }
+
+  animateWords();
+}
+runAnimation();
